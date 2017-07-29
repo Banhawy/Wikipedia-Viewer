81066829c7746a3c1063bbb4109973bf7e9cd06b
Banhawy <the.benhawy@gmail.com>
2017-07-28 20:57:36 -0500

    Added form event handlers

--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import Wiki from './Wiki';
 
 export default class Form extends Component {
-    state = {
-        query: undefined
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            query: '',
+            submittedQuery: undefined
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        event.preventDefault();
+        this.setState({
+            query: event.target.value
+        });
+    }
+    handleSubmit(event){
+        console.log('hi');
+        this.setState({
+            submittedQuery: event.target.value
+        })
 
+    } 
     render(){
+        if(this.state.submittedQuery !== undefined) return <h1>BOO</h1>
         return(
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input placeholder="Search Wikipedia"
-                        value={this.state.query}></input>
+                        value={this.state.query}
+                        onChange={this.handleChange}
+                        ></input>
+                        <input type="submit" value="Submit" />
             </form>
         )
     }
