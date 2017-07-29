ef601d4beb6480d5a47f114939ad2259eb3abfb5
Banhawy <the.benhawy@gmail.com>
2017-07-29 16:29:32 -0500

    Added rotating wiki logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import wiki_logo from './wiki_logo.png';
 import Wiki from './Wiki';
 import './App.css';
 
@@ -9,6 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-header">
+          <img src={wiki_logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Wiki Viewer</h2>
+        </div>
         <Wiki></Wiki>
       </div>
     );
