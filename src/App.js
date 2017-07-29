734630515efca8c183c52f26b6edf956ec885a6d
Banhawy <the.benhawy@gmail.com>
2017-07-29 15:35:00 -0500

    Removed all unused methids and constructor

--- src/App.js
+++ src/App.js
@@ -4,24 +4,13 @@ import Wiki from './Wiki';
 import Form from './Form';
 import './App.css';
 
-// <Github username="Banhawy"></Github>
-// onKeyPress={query => this.submit(query)}
-class App extends Component {
 
-  // onSubmit = query => {
-  //     return (
-  //       <div>
-  //         <Wiki query={query}></Wiki>
-  //       </div>
-  //     )
-  // }
+class App extends Component {
 
   render() {
     return (
       <div className="App">
-
-        <Form></Form>
-
+        <Wiki></Wiki>
       </div>
     );
   }
