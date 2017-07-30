c063011f81849561f0988767d1486257660964a2
Adham EL Banhawy <the.benhawy@gmail.com>
2017-07-30 10:39:56 -0500

    added dummy button needs styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+
 import wiki_logo from './wiki_logo.png';
 import Wiki from './Wiki';
 import './App.css';
 
+const style = {
+  margin: 12,
+};
 
 class App extends Component {
 
@@ -12,6 +18,13 @@ class App extends Component {
       <MuiThemeProvider>
       <div className="App">
         <div className="App-header">
+        <RaisedButton
+      href="https://github.com/callemall/material-ui"
+      target="_blank"
+      secondary={true}
+      icon={<FontIcon className="muidocs-icon-custom-github" />}
+      style={style}
+    />
           <img src={wiki_logo} className="App-logo" alt="logo" />
           <h2>Welcome to Wiki Viewer</h2>
         </div>
