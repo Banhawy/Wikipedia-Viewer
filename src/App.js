d543a0f1cbd38bd4c43f98afdf2373f2e8136e17
Adham EL Banhawy <the.benhawy@gmail.com>
2017-07-30 19:37:58 -0500

    Styled and positioned github button

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
-import {grey500} from 'material-ui/styles/colors';
+import {grey500, grey50, grey300} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
 
 import wiki_logo from './wiki_logo.png';
 import Wiki from './Wiki';
 import './App.css';
 
 const style = {
-  margin: 12
+  margin: 12,
+  float: 'right'
 };
 
 const githubIcon = <FontIcon className="fa fa-github" />;
+const githubButton = <FlatButton
+                      backgroundColor={grey300}
+                      hoverColor={grey500}
+                      href="https://github.com/callemall/material-ui"
+                      target="_blank"
+                      icon={githubIcon}
+                      style={style}
+                      />
 class App extends Component {
 
   render() {
@@ -20,14 +30,8 @@ class App extends Component {
       <MuiThemeProvider>
       <div className="App">
         <div className="App-header">
-        <RaisedButton
-          backgroundColor={grey500}
-          href="https://github.com/callemall/material-ui"
-          target="_blank"
-          icon={githubIcon}
-          style={style}
-          />
           <img src={wiki_logo} className="App-logo" alt="logo" />
+          {githubButton}
           <h2>Welcome to Wiki Viewer</h2>
         </div>
         <Wiki></Wiki>
