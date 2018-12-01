6dcaa170506235970f791dadddd46f11f92495a4
Adham PC <the.benhawy@gmail.com>
2018-12-01 21:44:44 +0200

    Added loading state and kept results on query change

--- src/Wiki.js
+++ src/Wiki.js
@@ -6,7 +6,7 @@ import {grey500} from 'material-ui/styles/colors';
 import FontAwesome from 'react-fontawesome';
 
 const apiUrl = query =>
-`https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${query}`
+`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${query}`
 
 const style = {
   margin: 12,
@@ -36,6 +36,7 @@ class Wiki extends Component {
         query: '',
         wikiData: undefined,
         requestFailed: false,
+        fetchingData: false,
         suggestions: [],
         title: null,
         description: [],
@@ -54,6 +55,10 @@ class Wiki extends Component {
     handleSubmit(event){
         console.log('hi');
         let query =  this.state.query;
+        let state = this.state;
+        this.setState({
+          fetchingData: true
+        })
         query ? this.getData(query) : false;
         event.preventDefault();
 
@@ -62,7 +67,7 @@ class Wiki extends Component {
     getData(){
       console.log('Before fetch')
         fetch(apiUrl(this.state.query))
-        .then(console.log('fetch successful'))
+        // .then(console.log('fetch successful'))
         .then(response => {
             if(!response.ok){
                 throw Error("msh sha3'ala!")
@@ -75,7 +80,8 @@ class Wiki extends Component {
                 suggestions: data[1],
                 title: data[0],
                 description: data[2],
-                links: data[3]
+                links: data[3],
+                fetchingData: false
             })
           console.log(data)
         }, ()=>{
@@ -87,6 +93,7 @@ class Wiki extends Component {
     render(){
         //Returns "Request Failed" if api fails to connect
         if(this.state.requestFailed) return <h1>Request Failed</h1>
+        if(this.state.fetchingData) return <h2>Loading...</h2>
         return (
           <div>
             <div className="container">
@@ -110,7 +117,7 @@ class Wiki extends Component {
             <div className="listings">
                 <ul>
                     {
-                    (this.state.query === this.state.title) ? this.state.description.map((item, index) =>
+                    (this.state.title) ? this.state.description.map((item, index) =>
                         {return (
                                 <ListItem key={index}
                                         title={this.state.suggestions[index]}
